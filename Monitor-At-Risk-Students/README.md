85926e87997f4f4778ec8b5f82b8cdcb18455676
Julius N. Mwangi <njmwas@gmail.com>
2024-03-12 23:36:27 +0300

    Monitor at risk students

---
+++ Monitor-At-Risk-Students/README.md
@@ -0,0 +1,4 @@
+# Monitor at risk students
+When you visit ```https://{domain}/courses/{courseId}/users you will see at risk students show like this
+
+![At risk stuents](/Monitor-At-Risk-Students/at-risk-flags.PNG)
---
+++ scripts/at-risk.js
@@ -0,0 +1,51 @@
+async function highlightAtRisk() {
+    const table = document.querySelector("table");
+    if (!table) {
+        setTimeout(highlightAtRisk, 1000);
+        return;
+    }
+
+    const course = await courseInfo();
+    const courseStartDate = new Date(course.start_at);
+    const now = new Date();
+    let expectedTime = now.getTime() - courseStartDate.getTime();
+    const expectedTimeOnCanvasPerDay = 4 * 60 * 60 * 1000;
+    // Remove the weekends time by getting the weeks the course takes minus hoursExpectedPerDay * 2days * weeks
+    const weeks = Math.floor(expectedTime / (7 * expectedTimeOnCanvasPerDay))
+    expectedTime -= expectedTimeOnCanvasPerDay * 2 * weeks;
+    
+    let expectedHours = Math.floor(expectedTime / expectedTimeOnCanvasPerDay);
+
+    table.querySelectorAll("tbody tr").forEach(row => {
+        let cols = row.querySelectorAll("td");
+        let role = cols[4].innerText;
+        
+        if(role !== "Student") return;
+
+        let studentName = cols[1].innerText;
+        let studentEmail = cols[2].innerText;
+        let timeColumn = cols[cols.length - 2]
+        // time spent
+        const [hr, min, sec] = timeColumn.innerText.split(":");
+        const pd = hr / expectedHours;
+        const div = timeColumn.querySelector('div');
+        div.style.padding = "5px"
+
+        if(pd <= 0.2) div.style.backgroundColor = `rgba(255, 0, 0, 1)`;
+        else if( pd <= 0.3 ) div.style.backgroundColor = `rgba(255, 0, 0, .4)`;
+        else if(pd <= 0.6) div.style.backgroundColor = `rgba(255, 255, 0, .4)`;
+
+    });
+}
+
+async function courseInfo() {
+    var url = window.location.href;
+    var urlParts = url.split("/");
+    if (urlParts.includes("courses")) {
+        var courseId = urlParts[urlParts.indexOf("courses") + 1];
+        return await fetch(`${window.location.protocol}//${window.location.host}/api/v1/courses/${courseId}`).then(resp => resp.json());
+    }
+    return null;
+}
+
+highlightAtRisk();
